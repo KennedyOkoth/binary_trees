076c26a7627dc81ce3a77fac020ad2117f81d76c
KennedyOkoth <107689277+KennedyOkoth@users.noreply.github.com>
2023-10-31 13:04:12 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,7 +22,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 
 	/* Allocate values to the node */
-	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
