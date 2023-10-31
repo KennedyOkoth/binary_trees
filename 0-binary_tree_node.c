98ecb12cbf5645968c00847c9da5b883f95872bf
KennedyOkoth <keinokoth.ok@gmail.com>
2023-10-31 03:41:10 -0700

    Updated necessary files.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,16 +18,16 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	/* Return NULL on failure */
 	if (new_node == NULL)
 	{
-		return (NULL);
+		return NULL;
 	}
 
 	/* Allocate values to the node */
- new_node->parent = parent;
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
 	/* Return pointer to the new node */
-	return (new_node);
+	return new_node;
 }
 
